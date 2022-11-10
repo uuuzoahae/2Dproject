f8d1c8f8a7d8ed0694e8f67c1ede8ebe6c9920d2
uuuzoahae <rlawlals010824@gmail.com>
2022-11-10 16:57:39 +0900

    Object Instantiation 2

--- boss_state.py
+++ boss_state.py
@@ -1,15 +1,15 @@
 from pico2d import *
 import game_framework
 
-boss_map = None
-running = None
-
 class Map():
     def __init__(self):
         self.image = load_image('boss_map.png')
     def draw(self):
         self.image.draw(300,300,600,600)
 
+
+boss_map = None
+running = None
 
 def enter():
     global running, boss_map
@@ -18,6 +18,8 @@ def enter():
     pass
 
 def exit():
+    global boss_map
+    del boss_map
     pass
 
 def draw():
