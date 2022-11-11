6b8a3808f3be1c92b4db1bea4d849b0d5894936c
uuuzoahae <rlawlals010824@gmail.com>
2022-11-11 14:21:16 +0900

    클래스 분리

---
+++ eevee.py
@@ -0,0 +1,26 @@
+from pico2d import *
+
+class eevee():
+    def __init__(self):
+        self.x,self.y = 300, 300
+        self.dir = 0
+        self.dirud = 0
+        self.frame = 0
+        self.image = load_image('character_eevee.png')
+        self.right = load_image('character_eevee_right.png')
+    def update(self):
+        self.frame = (self.frame + 1) % 3
+        self.x += self.dir * 5
+        self.y += self.dirud * 5
+    def draw(self):
+        # 상하
+        if self.dir == 0 and self.dirud > 0:
+            self.image.clip_draw(78 + self.frame * 25, 80, 25, 25, self.x, self.y, 40, 40)
+
+        # 정지 또는 걷기
+        elif self.dir == 0:
+            self.image.clip_draw(78 + self.frame * 25, 187, 25, 25, self.x, self.y, 40, 40)
+        elif self.dir > 0:
+            self.right.clip_draw(7 + self.frame * 24, 0, 25, 25, self.x, self.y, 40, 40)
+        elif self.dir < 0:
+            self.image.clip_draw(77 + self.frame * 25, 160, 25, 25, self.x, self.y, 40, 40)
