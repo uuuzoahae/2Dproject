1c904fcea2e4aedd27f9636b5647d390c771d88f
uuuzoahae <rlawlals010824@gmail.com>
2022-11-19 02:04:03 +0900

    boss_state 구현

--- boss_state.py
+++ boss_state.py
@@ -1,59 +1,101 @@
-from pico2d import *
+
+import game_framework
 import random
 import eevee
-import game_framework
+
+from pico2d import *
 from eevee import Eve
-from map import Map
-from light import Light
-from fire_ball import Ball
+from boss_map import Boss_Map
 from water_drop import Water_drop
-from mob import Mob
 import game_world
-import title_state
 import boss_state
 import play_state
 import time
+
 frame_time = 0.0
 
-map = None
 eve = None
 water = None
 many_water = None
 mob = None
 many_mob = None
-Balls = None
-count = 0
-
-
-class Map():
-    def __init__(self):
-        self.image = load_image('boss_map.png')
-    def draw(self):
-        self.image.draw(300,300,600,600)
-
-
 boss_map = None
-running = None
+
 
 def enter():
-    global running, boss_map
-    running = True
-    boss_map = Map()
-    pass
+    global map, eve, many_water, many_mob, water, frame_time, count
+    boss_map = Boss_Map()
+    eve = Eve()
+
+    # 게임 오브젝트 추가
+    game_world.add_object(eve, 1)
+    game_world.add_object(map, 0)
+
+    # 게임 충돌처리 추가
+    game_world.add_collision_pairs(eve, many_water, 'eve:water')
 
 def exit():
-    global boss_map
-    del boss_map
-    pass
+    game_world.clear()
+def update():
+    for game_object in game_world.all_objects():
+        game_object.update()
 
+
+    for a, b, group in game_world.all_collision_pairs():
+        if collide(a, b):
+            print('collision', group)
+            a.handle_collision(b, group)
+            b.handle_collision(a, group)
+
+def draw_world():
+    for game_object in game_world.all_objects():
+        game_object.draw()
 def draw():
     clear_canvas()
-    boss_map.draw()
+    draw_world()
     update_canvas()
+
+def pause():
     pass
 
-def update():
+def resume():
     pass
+
+def collide(a, b):
+    la, ba, ra, ta = a.get_bb()
+    lb, bb, rb, tb = b.get_bb()
+
+    if la > rb: return False
+    if ra < lb: return False
+    if ba > tb: return False
+    if ta < bb: return False
+
+    return True
+
+def test_self():
+    import play_state
+
+    pico2d.open_canvas()
+    game_framework.run(play_state)
+    pico2d.clear_canvas()
+
+if __name__ == '__main__':
+    test_self()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 def handle_events():
     events = get_events()
