1344b2c01f70c73bc474ee14a9cdc7bf9c6eb273
uuuzoahae <rlawlals010824@gmail.com>
2022-12-04 07:31:18 +0900

    eeveeui.py

--- eevee_ui.py
+++ eevee_ui.py
@@ -1,18 +1,17 @@
 from pico2d import *
+import game_framework
 class UI:
     hp_image = None
     light_image = None
-    def __init__(self):
+    def __init__(self, x, y, hp):
         if self.hp_image == None:
             self.hp_image = load_image('img/heart.py')
         if self.light_image == None:
             self.light_image = load_image('img/light_piece.py')
 
         self.frame = 0
-        self.x = 0
-        self.y = 0
-        self.gauge = 300
         self.inven = 0
+        self.x = x, self.y = y, self.hp = hp
         self.font = load_font('font/PKMN-Mystery-Dungeon.ttf',20)
 
     # def get_info(self, x, y, hp, light):
@@ -20,11 +19,12 @@ class UI:
     #     self.hp = hp, self.light = light
 
     def draw(self):
+        self.hp_image.clip_draw(int(self.frame),0,100,100,self.x, self.y+20)
         # self.hp_image.draw(300,300,300,300)
         pass
 
     def update(self):
-        self.get_info()
+        self.frame += game_framework.frame_time
         pass
 
     def handle_event(self):
